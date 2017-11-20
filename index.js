292bbfc4bd64fbe6d506ca4e803529c9a83da85d
offlainer <rangemind@mail.ru>
2017-11-20 18:36:08 +0300

    - twig as template engine - layout.twig is default template

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 /* Подключаем приложение */
 const express = require('express');
 const app = express();
-/* Инициализируем маршруты */
-
 // Включаем посредника для страницы аутентификации
 const auth = require('./routes/auth');
-
+// Устанавливаум twig как шаблонизатор по умолчанию
+app.set('views', './views');
+app.set('view engine', 'twig');
+/* Конфигурируем приложение */
+// Название приложения
+app.set('project_name', 'Social Booster');
+/* Инициализируем маршруты */
 // Роутим запрос авторизации
 app.use('/auth', auth);
 // Роутим дефолтный запрос
-app.get('/', (req, res) => res.send('Hello'));
+app.get('/', (req, res) => res.render('layout', { title : app.get('project_name')}));
 // Запускаем сервер
 app.listen(3000, () => console.log('Server is started'));
