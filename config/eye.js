3383a64c762c3678d06cdf7566478d894f79a035
lightkeeper <rangemind@mail.ru>
2017-12-03 01:27:55 +0300

    validate type of the output message type

--- config/eye.js
+++ config/eye.js
@@ -12,16 +12,28 @@ const eye = (unitName) => {
         name : unitName,
         /* logging info message */
         info : function (msg) {
+            if (typeof msg !== 'string') {
+                console.error('Incorrect message type for a stdout(info)');
+                process.exit(1);
+            }
             debug.log = console.info.bind(console);
             debug(`${this.name} => ${chalk.blue(msg)}`);
         },
         /* logging error message */
         err : function (msg, rsn = '') {
+            if (typeof msg !== 'string' || (typeof rsn !== 'string')) {
+                console.error('Incorrect message type for a stderr');
+                process.exit(1);
+            }
             debug.log = console.error.bind(console);
             debug(`${chalk.white(this.name)} => ${chalk.red(`${msg}; ${rsn ? chalk.white('Reason : ') + rsn : ''}`)}`);
         },
         /* logging success message */
         done : function (msg) {
+            if (typeof msg !== 'string') {
+                console.error('Incorrect message type for a stdout(done)');
+                process.exit(1);
+            }
             debug.log = console.info.bind(console);
             debug(`${this.name} => ${chalk.green(msg)}`);
         }
